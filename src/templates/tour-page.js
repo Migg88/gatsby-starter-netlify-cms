5f6608164944596e295b677810a42da42edcadd2
Migg88 <miguel141295@hotmail.com>
2019-08-09 17:00:41 -0500

    retry template

--- src/templates/tour-page.js
+++ src/templates/tour-page.js
@@ -4,3 +4,29 @@ import { graphql, Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+
+export const tourComponent = ({
+	content,
+	contentComponent,
+	helmet,
+}) => {
+
+	const tourContent = contentComponent || Content
+
+	return(
+
+		<p>works</p>
+
+	)
+
+}
+
+export const tourQuery = graphql`
+	query TourPage($id: String!){
+		markdownRemark(id: { eq: $id }) {
+			frontmatter {
+				title
+			}
+		}
+	}
+`
